f5bbe9bfa2468ab35395f4228aa180d03c6de9f1
RafiMaulana22 <rafi.maulana211@gmail.com>
2025-01-30 23:26:56 +0700

    feat: update template

--- routes/web.php
+++ routes/web.php
@@ -9,15 +9,11 @@ Route::get('/', function () {
 });
 
 // rafi
-<<<<<<< HEAD
-Route::get('/kegiatan/{kegiatan}', [])->name('kegiatan.show');
-=======
 Route::get('/kegiatan', [KegiatanController::class, 'index'])->name('kegiatan.index');
 // nanda
 
 
 
->>>>>>> ba35c9f2f494845aa2ad8958b15734f7645efe51
 
 // start nanda
 Route::get('login', [AuthController::class, 'login'])->name('login');
