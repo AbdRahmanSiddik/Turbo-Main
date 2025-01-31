41cf328f1de8d752e2ce7a3c6e8094161843ac9f
Abd. Rahman Siddik <siddikrahmanabd17@gmail.com>
2025-01-31 20:38:41 +0700

    feat(pendaftran): tambah migration, model, controller

---
+++ app/Http/Controllers/PendaftaranController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pendaftaran;
+use App\Http\Requests\PendaftaranRequest as Request;
+
+class PendaftaranController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Pendaftaran $pendaftaran)
+    {
+        //
+    }
+
+    public function edit(Pendaftaran $pendaftaran)
+    {
+        //
+    }
+
+    public function update(Request $request, Pendaftaran $pendaftaran)
+    {
+        //
+    }
+
+    public function destroy(Pendaftaran $pendaftaran)
+    {
+        //
+    }
+}
