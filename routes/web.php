1263ac71fba268f240ca725c1c62dbbfca1e6a48
anandamw <ananda.maulana.w@gmail.com>
2025-02-01 18:41:55 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,18 @@
 <?php
 
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-<<<<<<< HEAD
-use App\Http\Controllers\KegiatanController;
-use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\RoleController;
 
-use Illuminate\Support\Facades\Route;
-=======
-use App\Http\Controllers\UsersManagement;
-use App\Http\Controllers\RolePermissionController;
 use App\Http\Controllers\TeamController;
->>>>>>> 38e368f7931362b1e725c9900339e6695890f1eb
+use App\Http\Controllers\KegiatanController;
+use App\Http\Controllers\PermissionController;
 
 // rafi
-// nanda
 
+
+// start nanda
 Route::middleware('guest')->group(function () {
     Route::get('/kegiatan', [KegiatanController::class, 'index'])->name('kegiatan.index');
     Route::view('/', 'auth.login')->name('login');
@@ -23,23 +20,25 @@ Route::middleware('guest')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
 });
 
-// start nanda
+
 
 Route::middleware(['auth', 'role_permission'])->group(function () {
     Route::get('dashboard', function () {
         return view('admin.dashboard.dashboard-view');
     })->name('dashboard');
 
+
     Route::get('roles', [RoleController::class, 'index'])->name('roles.index');
     Route::post('roles/store', [RoleController::class, 'store'])->name('roles.store');
     Route::post('/roles/{id}/update', [RoleController::class, 'update'])->name('roles.update');
     Route::get('/roles/destroy/{id}', [RoleController::class, 'destroy'])->name('roles.destroy');
-    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
     Route::get('permissions', [PermissionController::class, 'index'])->name('permissions.index');
     Route::post('permissions/store', [PermissionController::class, 'storePermission'])->name('permissions.store');
     Route::post('permissions/{id}/update', [PermissionController::class, 'updatePermission'])->name('permissions.update');
     Route::get('permissions/destroy/{id}', [PermissionController::class, 'destroyPermission'])->name('permissions.destroy');
+
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
 
 // end nanda
@@ -53,4 +52,3 @@ Route::get('/team', [TeamController::class, 'index'])->name('team.index');
 Route::post('/team/store', [TeamController::class, 'store'])->name('team.store');
 Route::get('/team/update/{id}', [TeamController::class, 'update'])->name('team.update');
 Route::post('/team/destroy{id}', [TeamController::class, 'destroy'])->name('team.destroy');
-
